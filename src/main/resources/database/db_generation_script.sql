e2e4f847bc6159d63e7935d5b40c96113b9a6029
vlad_yukharin <yukharinvlad@gmail.com>
2019-09-09 19:13:09 +0500

    Second commit

--- src/main/resources/database/db_generation_script.sql
+++ src/main/resources/database/db_generation_script.sql
@@ -54,3 +54,35 @@ CREATE TABLE IF NOT EXISTS `rest_service`.`cars`
 SET SQL_MODE = @OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `rest_service`.`persons`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `rest_service`;
+INSERT INTO `rest_service`.`persons` (`id`, `name`, `birthdate`)
+VALUES (1, 'Vlad', '1998-07-21');
+INSERT INTO `rest_service`.`persons` (`id`, `name`, `birthdate`)
+VALUES (2, 'Slava', '1997-06-21');
+INSERT INTO `rest_service`.`persons` (`id`, `name`, `birthdate`)
+VALUES (3, 'Kevin', '1996-05-24');
+INSERT INTO `rest_service`.`persons` (`id`, `name`, `birthdate`)
+VALUES (4, 'Karen', '1995-04-12');
+INSERT INTO `rest_service`.`persons` (`id`, `name`, `birthdate`)
+VALUES (5, 'Stas', '1996-05-09');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `rest_service`.`cars`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `rest_service`;
+INSERT INTO `rest_service`.`cars` (`id`, `model`, `horsepower`, `id_person`)
+VALUES (1, 'BMW-X5', 600, 1);
+INSERT INTO `rest_service`.`cars` (`id`, `model`, `horsepower`, `id_person`)
+VALUES (2, 'BMW-X6', 650, 1);
+
+COMMIT;
+
