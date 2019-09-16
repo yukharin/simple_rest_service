4e540ab338656a4ff2799d93b96c50e34fdc2d8e
vlad_yukharin <yukharinvlad@gmail.com>
2019-09-16 19:54:22 +0500

    Some refactoring

--- src/test/java/CarServiceTest.java
+++ src/test/java/CarServiceTest.java
@@ -1,12 +1,15 @@
 import com.lanit.simple_rest_service.entities.Car;
 import com.lanit.simple_rest_service.entities.Person;
 import com.lanit.simple_rest_service.repositories.CarRepository;
+import com.lanit.simple_rest_service.repositories.PersonRepository;
 import com.lanit.simple_rest_service.services.CarService;
 import org.junit.jupiter.api.*;
 
+import javax.validation.Validator;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,14 +18,19 @@ import static org.mockito.Mockito.when;
 public class CarServiceTest {
 
     private static List<Car> cars;
+    private static Car car;
     private static CarService carService;
+    private static CarRepository carRepository;
+    private static PersonRepository personRepository;
+    private static Validator validator;
 
 
     @BeforeAll
     public static void setUpAll() {
-        CarRepository mockCarRepository = mock(CarRepository.class);
-        Car car = new Car();
-        when(mockCarRepository.findAll()).thenReturn(cars);
+        carRepository = mock(CarRepository.class);
+        personRepository = mock(PersonRepository.class);
+        validator = mock(javax.validation.Validator.class);
+        carService = new CarService(carRepository, personRepository, validator);
     }
 
     @AfterAll
@@ -46,7 +54,9 @@ public class CarServiceTest {
 
 
     @Test
-    public void testCar() {
+    public void testFindCarById() {
+        when(carRepository.findByOwnerId(1L)).thenReturn(Optional.of())
+        carService.findByOwnerId()
     }
 }
 
