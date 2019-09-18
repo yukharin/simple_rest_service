ef7712f394d6a72e5e2b0bae1e263b29d43e1570
vlad_yukharin <yukharinvlad@gmail.com>
2019-09-18 13:53:58 +0500

    Added a lot of tests

--- src/main/resources/database/db_generation_script.sql
+++ src/main/resources/database/db_generation_script.sql
@@ -1,10 +1,6 @@
 -- MySQL Workbench Forward Engineering
-#
-# SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
-# SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-# SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
-#         'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+SET MODE MYSQL;
 -- -----------------------------------------------------
 -- Schema rest_service
 -- -----------------------------------------------------
@@ -49,12 +45,6 @@ CREATE TABLE IF NOT EXISTS `rest_service`.`cars`
             ON UPDATE CASCADE
 )
     ENGINE = InnoDB;
-
-
-# SET SQL_MODE = @OLD_SQL_MODE;
-# SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-# SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
-
 -- -----------------------------------------------------
 -- Data for table `rest_service`.`persons`
 -- -----------------------------------------------------
